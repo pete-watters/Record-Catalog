c933a75b9ca750b2c9f8be8df9e81fb428277fa7
Peter Watters <a552722@fmr.com>
2014-10-16 12:10:45 +0100

    Commit initial Yeoman structure

---
+++ build/templates.js
@@ -0,0 +1,10 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["app/templates/main-layout.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<p>\n	Handlebar templated rendered!\n</p>";
+  });
